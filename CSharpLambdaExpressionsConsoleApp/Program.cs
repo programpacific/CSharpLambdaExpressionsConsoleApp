919fdb2f12487b8d19751afdbe109e7ef0eb6670
programpacific <programpacific@users.noreply.github.com>
2018-08-02 18:05:59 -0600

    Added exercise parameters.

--- CSharpLambdaExpressionsConsoleApp/Program.cs
+++ CSharpLambdaExpressionsConsoleApp/Program.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace CSharpLambdaExpressionsConsoleApp
 {
+
+    //1. In the Main() method, create a list of at least 10 employees.Each employee should have a first and last name, as well as an Id.At least two employees should have the first name "Joe".
+    //2. Using a foreach loop, create a new list of all employees with the first name "Joe".
+    //3. Do the same thing again, but this time with a lambda expression.
+    //4. Using a lambda expression, make a list of all employees with an Id number greater than 5.
+
     class Program
     {
         static void Main(string[] args)
